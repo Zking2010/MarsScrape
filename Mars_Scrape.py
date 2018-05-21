50c11cfbbf1a6fe9f22ff1207d602bb79234b177
Zking2010 <Zachk2010@gmail.com>
2018-05-21 18:50:47 -0400

    final

--- Mars_Scrape.py
+++ Mars_Scrape.py
@@ -30,72 +30,76 @@ url_4 = 'http://space-facts.com/mars/'
 url_5 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
 
 
-    
 # In[4]:
-def scrape():
-    browser=init_browser()
-    browser.visit(url_1)
-    title= browser.find_by_css('.content_title').first.text
-    news= browser.find_by_css('.article_teaser_body').first.text
 
+
+browser=init_browser()
+browser.visit(url_1)
+title= browser.find_by_css('.content_title').first.text
+news= browser.find_by_css('.article_teaser_body').first.text
+print(title, "\n" + news)
 
 
 # In[5]:
 
 
-    browser=init_browser()
-    browser.visit(url_2)
-    browser.find_by_id('full_image').click()
-    featured_image=browser.find_by_css('.fancybox-image').first['src']
-
+browser=init_browser()
+browser.visit(url_2)
+browser.find_by_id('full_image').click()
+featured_image=browser.find_by_css('.fancybox-image').first['src']
+print(featured_image)
 
 
 # In[6]:
 
 
-    browser=init_browser()
-    browser.visit(url_3)
-    for text in browser.find_by_css('.tweet-text'):
-        if text.text.partition(" ")[0] == 'Sol':
-            weather = text.text
-        break
+browser=init_browser()
+browser.visit(url_3)
+for text in browser.find_by_css('.tweet-text'):
+    if text.text.partition(" ")[0] == 'Sol':
+        weather = text.text
+    break
     
 
-
+print(weather)
 
 
 # In[7]:
 
 
-    df= pd.read_html(url_4, attrs = {'id': 'tablepress-mars'})[0]
-    df = df.set_index(0).rename(columns={1:'value'})
-    del df.index.name
-    facts = df.to_html()
-   
+df= pd.read_html(url_4, attrs = {'id': 'tablepress-mars'})[0]
+df = df.set_index(0).rename(columns={1:'value'})
+del df.index.name
+facts = df.to_html()
+print(facts)
 
 
 # In[8]:
 
 
-    hemispheres= requests.get(url_5)
-    soup= BeautifulSoup(hemispheres.text,"html.parser")
-    hemisphere_list = soup.find_all('a', class_= "itemLink product-item")
+hemispheres= requests.get(url_5)
+soup= BeautifulSoup(hemispheres.text,"html.parser")
+hemisphere_list = soup.find_all('a', class_= "itemLink product-item")
+hemisphere_list
 
+
+# In[9]:
+
+
+hemisphere_img = []
+for img in hemisphere_list:
+    img_title = img.find('h3').text
+    link = "https://astrogeology.usgs.gov/" + img["href"]
+    img_request= requests.get(link)
+    soup_= BeautifulSoup(img_request.text, 'html.parser')
+    img_tag= soup_.find('div',class_='downloads')
+    img_url= img_tag.find('a')['href']
+    hemisphere_img.append({"Title":img_title, "Url": img_url})
+
+hemisphere_img
 
 
 # In[10]:
-
-
-    hemisphere_img = []
-    for img in hemisphere_list:
-        img_title = img.find('h3').text
-        link = "https://astrogeology.usgs.gov/" + img["href"]
-        img_request= requests.get(link)
-        soup_= BeautifulSoup(img_request.text, 'html.parser')
-        img_tag= soup_.find('div',class_='downloads')
-        img_url= img_tag.find('a')['href']
-        hemisphere_img.append({"Title":img_title, "Url": img_url})
-
 
 
 Mars_scrape = {
@@ -104,5 +108,6 @@ Mars_scrape = {
     "Mars_Image": featured_image,
     "Mars_Weather":weather,
     "Mars_Facts":facts,
-    "Mars_Hemisphere":hemisphere_img
+    "Hemishpere_Image":hemisphere_img
 }
+
--- mars.py
+++ mars.py
@@ -1,3 +1,35 @@
-from Flask import Flask, jsonify, render_template, redirect
+from flask import Flask, jsonify, render_template, redirect
 from flask_pymongo import pymongo
+import Mars_Scrape
+import os
+from pymongo import MongoClient
+
+app = Flask( __name__)
+
+mongo = pymongo(app)
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars = mars)
+
+@app.route('/scrape')
+def scrape():
+    mars_data = mongo.db.mars
+    data = Mars_Scrape.scrape()
+
+    mars_data.insert_one(data)
+
+    data.pop('_id',None)
+
+    mars_data.update(
+        {},
+        data,
+        upsert=True
+    )
+
+    return redirect("http://localhost:5000/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
